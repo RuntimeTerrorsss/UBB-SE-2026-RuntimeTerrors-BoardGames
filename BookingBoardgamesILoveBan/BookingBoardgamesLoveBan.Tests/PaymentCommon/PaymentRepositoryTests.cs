f48c809767143ec8871155ed07a36bb00940f7ae
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-04-14 19:21:59 +0300

    all tests without db

---
+++ BookingBoardgamesILoveBan/BookingBoardgamesLoveBan.Tests/PaymentCommon/PaymentRepositoryTests.cs
@@ -0,0 +1,38 @@
+﻿using BookingBoardgamesILoveBan.Src.PaymentCommon.Model;
+using BookingBoardgamesILoveBan.Src.PaymentCommon.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingBoardgamesLoveBan.Tests.PaymentCommon
+{
+    public class PaymentRepositoryTests // integration tests idk how
+    {
+        private readonly IPaymentRepository paymentRepository;
+
+        public PaymentRepositoryTests()
+        {
+            paymentRepository = new PaymentRepository();
+        }
+
+        private Payment CreatePayment()
+        {
+            return new Payment
+            {
+                RequestId = 1,
+                ClientId = 1,
+                OwnerId = 2,
+                Amount = 99.99m,
+                PaymentMethod = "Card",
+                State = 0,
+                DateOfTransaction = DateTime.Now,
+                DateConfirmedBuyer = null,
+                DateConfirmedSeller = null,
+                FilePath = null
+            };
+        }
+
+    }
+}
---
+++ BookingBoardgamesILoveBan/BookingBoardgamesLoveBan.Tests/PaymentHistory/ServicePaymentTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingBoardgamesLoveBan.Tests.PaymentHistory
+{
+    public class ServicePaymentTests // TODO
+    {
+    }
+}
