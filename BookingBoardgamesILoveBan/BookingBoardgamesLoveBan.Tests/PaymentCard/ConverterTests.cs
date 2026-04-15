e81625fe3c24765d1cd6e177a99a8a528d6edf90
György-Mózes Áron <aron.gyorgy@stud.ubbcluj.ro>
2026-04-15 20:35:38 +0300

    Added more tests

---
+++ BookingBoardgamesILoveBan/BookingBoardgamesLoveBan.Tests/PaymentCard/ConverterTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.UI.Xaml;
+using Xunit;
+using BookingBoardgamesILoveBan.Src.PaymentCard.Converters;
+
+namespace BookingBoardgamesILoveBan.Tests.PaymentCard.Converters
+{
+    public class ConverterTests
+    {
+        [Fact]
+        public void BoolToVisibility_True_ReturnsVisible()
+        {
+            var converter = new BoolToVisibilityConverter();
+            var result = converter.Convert(true, typeof(Visibility), null, null);
+            Assert.Equal(Visibility.Visible, result);
+        }
+
+        [Fact]
+        public void BoolToVisibility_False_ReturnsCollapsed()
+        {
+            var converter = new BoolToVisibilityConverter();
+            var result = converter.Convert(false, typeof(Visibility), null, null);
+            Assert.Equal(Visibility.Collapsed, result);
+        }
+
+        [Fact]
+        public void InverseBoolToVisibility_True_ReturnsCollapsed()
+        {
+            var converter = new InverseBoolToVisibilityConverter();
+            var result = converter.Convert(true, typeof(Visibility), null, null);
+            Assert.Equal(Visibility.Collapsed, result);
+        }
+
+        [Fact]
+        public void InverseBoolToVisibility_False_ReturnsVisible()
+        {
+            var converter = new InverseBoolToVisibilityConverter();
+            var result = converter.Convert(false, typeof(Visibility), null, null);
+            Assert.Equal(Visibility.Visible, result);
+        }
+    }
+}
